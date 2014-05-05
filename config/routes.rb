79f4585e0a5e4756ea98925c88822af8bcf53030
spencereldred <eldredspencer@gmail.com>
2014-05-05 08:22:29 -0700

    Add registration sign_up rspec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,40 @@
 Hi5::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
                      controllers: { omniauth_callbacks: "omniauth_callbacks"}
-                     
+
   get "landing/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'landing#index'
+
+# spencers-mbp-2:Hi5 Spencer$ rake routes
+#                   Prefix Verb     URI Pattern                            Controller#Action
+#         new_user_session GET      /users/login(.:format)                 devise/sessions#new
+#             user_session POST     /users/login(.:format)                 devise/sessions#create
+#     destroy_user_session DELETE   /users/logout(.:format)                devise/sessions#destroy
+#  user_omniauth_authorize GET|POST /users/auth/:provider(.:format)        omniauth_callbacks#passthru {:provider=>/twitter|facebook/}
+#   user_omniauth_callback GET|POST /users/auth/:action/callback(.:format) omniauth_callbacks#(?-mix:twitter|facebook)
+#            user_password POST     /users/password(.:format)              devise/passwords#create
+#        new_user_password GET      /users/password/new(.:format)          devise/passwords#new
+#       edit_user_password GET      /users/password/edit(.:format)         devise/passwords#edit
+#                          PATCH    /users/password(.:format)              devise/passwords#update
+#                          PUT      /users/password(.:format)              devise/passwords#update
+# cancel_user_registration GET      /users/cancel(.:format)                devise/registrations#cancel
+#        user_registration POST     /users(.:format)                       devise/registrations#create
+#    new_user_registration GET      /users/sign_up(.:format)               devise/registrations#new
+#   edit_user_registration GET      /users/edit(.:format)                  devise/registrations#edit
+#                          PATCH    /users(.:format)                       devise/registrations#update
+#                          PUT      /users(.:format)                       devise/registrations#update
+#                          DELETE   /users(.:format)                       devise/registrations#destroy
+#            landing_index GET      /landing/index(.:format)               landing#index
+#                     root GET      /                                      landing#index
+
+
+
+
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
-describe User do  
+describe User do 
+
+  before :each do 
+    @user = FactoryGirl.build(:user)
+  end 
 
   describe "email" do 
 
-    before :each do 
-      @user = FactoryGirl.build(:user)
+    it "should accept valid email format" do 
+      @user.should be_valid 
     end
 
     it "should not be empty" do
       user = FactoryGirl.build(:user, email: nil)
       user.should_not be_valid   
-    end
-
-    it "should accept valid email format" do 
-      user = FactoryGirl.build(:user)
-      user.should be_valid 
     end
 
     it "should be unique" do 
@@ -28,6 +27,37 @@ describe User do
       user = FactoryGirl.build(:user, email: '123.com')
       user.should_not be_valid 
     end
+
+  end
+
+  describe "password" do 
+
+    it "should change password" do 
+      @user.password = "newpassword"
+      @user.password_confirmation = "newpassword"
+      @user.save
+      @user.should be_valid
+    end
+
+    it "should not change password - too short" do 
+      @user.password = "short"
+      @user.password_confirmation = "short"
+      @user.save
+      @user.should_not be_valid
+    end
+
+    it "should not be valid password does not exist" do  
+      @user.password = nil
+      @user.save
+      @user.should_not be_valid
+    end
+
+    it "should not be valid password_confirmation does match" do  
+      @user.password_confirmation = 'no_match'
+      @user.save
+      @user.should_not be_valid
+    end
+
 
   end
 
