f1268cf84853a38c4c10e4eb0f51014654a0e47a
spencereldred <eldredspencer@gmail.com>
2014-05-05 06:34:24 -0700

    Add rspec tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,17 @@ describe User do
       user.should be_valid 
     end
 
+    it "should be unique" do 
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.build(:user, email: user1.email)
+      user2.should_not be_valid 
+    end
+
+    it "should reject invalid email format" do 
+      user = FactoryGirl.build(:user, email: '123.com')
+      user.should_not be_valid 
+    end
+
   end
 
 end
--- spec/requests/landing_spec.rb
+++ spec/requests/landing_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-describe 'Landing Page' do 
+describe 'Requests' do 
+  describe 'Landing Page' do 
 
-  before(:each) do  
-    get "/"
-  end  
-  
-  it 'should be a valid response' do
-    expect(response.status).to eq(200) 
+    before(:each) do  
+      get "/"
+    end  
+    
+    it 'should be a valid response' do
+      expect(response.status).to eq(200) 
+    end
+
+    it 'should render template index' do 
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include "Landing" on the page' do 
+      expect(response.body).to include("Landing")
+    end
+
   end
-
-  it 'should render template index' do 
-    expect(response).to render_template(:index)
-  end
-
-  it 'should include "Landing" on the page' do 
-    expect(response.body).to include("Landing")
-  end
-
 end
