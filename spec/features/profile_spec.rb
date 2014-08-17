03a3435f40c8e43ae56a39205c27cfe9ece3aa24
Spencer Eldred <eldredspencer@gmail.com>
2014-08-17 04:18:30 -0600

    Add capybara test for login

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -1,25 +1,31 @@
-# # require 'capybara/rails'
-#
-# feature 'Task lists' do
-#
-#   before do
-#     click_on "Sign Up"
-#
-#
-#   end
-#
-#   after do
-#
-#   end
-#
-#   scenario "login goes to home page" do
-#
-#     expect(page).to have_content("Sign Up")
-#
-#
-#
-#   end
-#
-#
-#
-# end
+require 'capybara/rails'
+
+feature 'Task lists' do
+
+  before do
+    visit new_user_session_path
+    click_link "Sign up"
+    expect(page).to have_content("Password confirmation")
+    fill_in "Email", with: 'user@example.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign up"
+
+
+  end
+
+  after do
+
+  end
+
+  scenario "login goes to home page" do
+    expect(page).to have_content("Landing Page")
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+
+
+
+  end
+
+
+
+end
