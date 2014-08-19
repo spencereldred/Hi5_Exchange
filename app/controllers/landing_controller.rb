3b02267209277f71c14188cf3eb51217f8e2c7d6
Spencer Eldred <eldredspencer@gmail.com>
2014-08-19 12:07:28 -0600

    Add detection for profile of new user: current_user.profile.blank?

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,17 @@
 class LandingController < ApplicationController
   def index
     @users = User.all
+    if current_user.profile.blank?
+      # go to profile new page
+      # depending on what function - go to single page app of that function
+      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@ we need a profile!"
+    elsif current_user.profile.function == "recycler"
+      # go to recycler single page app
+      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@ I am a recycler!"
+    elsif current_user.profile.function == "redeemer"
+      # go to redeemer single page app
+      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@ I am a redeemer!"
+    end
+
   end
 end
