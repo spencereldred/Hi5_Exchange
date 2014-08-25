23d3bbcedd497da5785a031910c4cf3f32555c8f
Spencer Eldred <eldredspencer@gmail.com>
2014-08-25 03:07:29 -0600

    Modify seeds file, simplify email addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-red1_co = User.create(
-  email: 'red1_co@example.com', password: 'password',
+red1 = User.create(
+  email: 'red1@example.com', password: 'password',
   password_confirmation: 'password')
 
-red1_co_profile = Profile.create(
-  user_id: red1_co.id, first_name: 'Joe-redeemer', last_name: 'Colorado',
+red1_profile = Profile.create(
+  user_id: red1.id, first_name: 'Joe-redeemer', last_name: 'Colorado',
   address: '460 Humboldt St.', city: 'Denver', state: 'CO',
   zipcode: '80209', phone: '8082803758',function: 'redeemer')
 
-rec1_co = User.create(
-  email: 'rec1_co@example.com', password: 'password',
+rec1 = User.create(
+  email: 'rec1@example.com', password: 'password',
   password_confirmation: 'password')
 
-rec1_co_profile = Profile.create(
-  user_id: rec1_co.id, first_name: 'Joe-recycler', last_name: 'Colorado',
+rec1_profile = Profile.create(
+  user_id: rec1.id, first_name: 'Joe-recycler', last_name: 'Colorado',
   address: '1062 Delaware St.', city: 'Denver', state: 'CO',
   zipcode: '80204', phone: '8082803758',function: 'recycler')
 
-red2_sf = User.create(
-  email: 'red2_sf@example.com', password: 'password',
+red2 = User.create(
+  email: 'red2@example.com', password: 'password',
   password_confirmation: 'password')
 
-red2_sf_profile = Profile.create(
-  user_id: red2_sf.id, first_name: 'Joe-redeemer', last_name: 'SanFrancisco',
+red2_profile = Profile.create(
+  user_id: red2.id, first_name: 'Joe-redeemer', last_name: 'SanFrancisco',
   address: '414 Brannan St.', city: 'San Francisco', state: 'CA',
   zipcode: '94107', phone: '8082803758',function: 'redeemer')
 
-rec2_sf = User.create(
-  email: 'rec2_sf@example.com', password: 'password',
+rec2 = User.create(
+  email: 'rec2@example.com', password: 'password',
   password_confirmation: 'password')
 
-rec2_sf_profile = Profile.create(
-  user_id: rec2_sf.id, first_name: 'Joe-recycler', last_name: 'SanFrancisco',
+rec2_profile = Profile.create(
+  user_id: rec2.id, first_name: 'Joe-recycler', last_name: 'SanFrancisco',
   address: '268 St. Charles Ave.', city: 'San Francisco', state: 'CA',
   zipcode: '94132', phone: '8082803758',function: 'recycler')
 
@@ -52,10 +52,10 @@ sf_recyclable4 = Recyclable.create(trans_type: "samaritan", cardboard: true,
   magazines: false, paper: true, newspaper: false)
 
 user_recyclable = UserRecyclable.create([
-  {user_id: rec1_co.id , recyclable_id: co_recyclable1.id, redeemer_id: red1_co.id },
-  {user_id: rec1_co.id , recyclable_id: co_recyclable2.id, redeemer_id: red1_co.id },
-  {user_id: rec2_sf.id , recyclable_id: sf_recyclable3.id, redeemer_id: red2_sf.id },
-  {user_id: rec2_sf.id , recyclable_id: sf_recyclable4.id, redeemer_id: red2_sf.id }
+  {user_id: rec1.id , recyclable_id: co_recyclable1.id, redeemer_id: red1.id },
+  {user_id: rec1.id , recyclable_id: co_recyclable2.id, redeemer_id: red1.id },
+  {user_id: rec2.id , recyclable_id: sf_recyclable3.id, redeemer_id: red2.id },
+  {user_id: rec2.id , recyclable_id: sf_recyclable4.id, redeemer_id: red2.id }
   ])
 
 
