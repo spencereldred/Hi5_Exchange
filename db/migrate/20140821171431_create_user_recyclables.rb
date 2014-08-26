efd570d487ea0ccc53a47f84eaa21f596acde62e
Spencer Eldred <eldredspencer@gmail.com>
2014-08-26 10:50:36 -0600

    fix migration

--- db/migrate/20140821171431_create_user_recyclables.rb
+++ db/migrate/20140821171431_create_user_recyclables.rb
@@ -3,7 +3,6 @@ class CreateUserRecyclables < ActiveRecord::Migration
     create_table :user_recyclables do |t|
       t.references :user
       t.references :recyclable
-      t.integer    :redeemer_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821171431) do
+ActiveRecord::Schema.define(version: 20140822151102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,9 @@ ActiveRecord::Schema.define(version: 20140821171431) do
   create_table "user_recyclables", force: true do |t|
     t.integer  "user_id"
     t.integer  "recyclable_id"
-    t.integer  "redeemer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "redeemer_id"
   end
 
   create_table "users", force: true do |t|
