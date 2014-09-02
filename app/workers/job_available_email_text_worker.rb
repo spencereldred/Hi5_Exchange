0765db382f6f44356e470445a803dac71ffee3be
Spencer Eldred <eldredspencer@gmail.com>
2014-09-02 01:45:00 -1000

    Add Sidekiq worker for email and text job creation notificaiton - notify within user's radius

--- app/workers/job_available_email_text_worker.rb
+++ app/workers/job_available_email_text_worker.rb
@@ -4,18 +4,19 @@ class JobAvailableEmailTextWorker
   def perform(recycler_user_id)
     max_radius = 20 # set the notification radius
     user = User.find(recycler_user_id)
-    redeemers_within_max_radius = User.near([user.profile.latitude, user.profile.longitude], max_radius).where(function: "redeemer")
+    user_profile = user.profile
+    profiles_within_max_radius = Profile.near([user.profile.latitude, user.profile.longitude], max_radius).where(function: "redeemer")
 
-    redeemers_within_max_radius.each do |near_redeemer|
-      if user.distance_from([near_redeemer.profile.latitude, near_redeemer.profile.longitude]) <= near_redeemer.profile.radius
+    profiles_within_max_radius.each do |profile|
+      if user_profile.distance_from([profile.latitude, profile.longitude]) <= profile.radius
         ############ Transaction Created ##############
         # send all redeemers within their radius an alert that a job has been posted
-        Hi5Mailer.job_available(near_redeemer).deliver
+        Hi5Mailer.job_available(profile.user).deliver
         # send text message alert
-        message = "Shaka! #{near_redeemer.profile.first_name}, a recycle job has just been posted in your area.
+        message = "Shaka! #{profile.first_name}, a recycle job has just been posted in your area.
         Questions, contact Annie at hi5exchange@gmail.com."
-        send_text(message, near_redeemer.profile.phone ) if !near_redeemer.profile.phone.empty?
-        puts "@@@@@@@@@@@@@@@@@@@@@@@@@@ distance #{near_redeemer.profile.first_name} is : #{user.distance_from([near_redeemer.profile.latitude, near_redeemer.profile.longitude])}"
+        send_text(message, profile.phone ) if !profile.phone.empty?
+        puts "@@@@@@@@@@@@@@@@@@@@@@@@@@ distance #{profile.first_name} is : #{user_profile.distance_from([profile.latitude, profile.longitude])}"
       end
     end
   end
