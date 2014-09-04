34e3f3febb3026f39298a87829514c3d00d84468
Spencer Eldred <eldredspencer@gmail.com>
2014-09-04 02:51:07 -1000

    Add transactions to redeemer#index via AngularJS

--- app/controllers/redeemers_controller.rb
+++ app/controllers/redeemers_controller.rb
@@ -6,8 +6,8 @@ class RedeemersController < ApplicationController
     # for testing geocoder in rails console:
     # @redeemables = Recyclable.where(trans_type: "redeemable", selected: false, completed: false).near([user.profile.latitude, user.profile.longitude], user.profile.radius)
 
-    @redeemables_selected = Recyclable.where(selected_redeemer_id: current_user.id, trans_type: "redeemable", selected: true, completed: false)
-    @samaritans_selected = Recyclable.where(selected_redeemer_id: current_user.id, trans_type: "samaritan", selected: true, completed: false)
+    @redeemables_selected = Recyclable.where(selected_redeemer_id: current_user.id, trans_type: "redeemable", selected: true, completed: false).near([current_user.profile.latitude, current_user.profile.longitude], current_user.profile.radius)
+    @samaritans_selected = Recyclable.where(selected_redeemer_id: current_user.id, trans_type: "samaritan", selected: true, completed: false).near([current_user.profile.latitude, current_user.profile.longitude], current_user.profile.radius)
     trans = Recyclable.near([current_user.profile.latitude, current_user.profile.longitude], current_user.profile.radius)
     respond_to do |format|
       format.html
