8c714e7a58e029795fc73f1955f59655c99200ad
Spencer Eldred <eldredspencer@gmail.com>
2015-12-06 10:27:57 -1000

    cofigure production.rb 'config.assets.compile = true'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Hi5::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
